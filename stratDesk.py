53565044f79a71f59427dc8eb17780565ba3e5e3
SabbaKix <9255011+sabbakix@users.noreply.github.com>
2021-11-16 16:58:45 +0100

    First commit

--- stratDesk.py
+++ stratDesk.py
@@ -1,17 +1,26 @@
+
+
+
+from  configparser import ConfigParser
 from wakeonlan import send_magic_packet
 import os
 import time
-from tcping import Ping
+#from tcping import Ping
+
+# read configurations
+config = ConfigParser()
+config.read('settings.ini')
+print(config['pc']['name'])
+print(config['pc']['mac'])
+
 
 # packetto in Broadcast su port 9
-
-
-#katia
-print("Katia-pc ... ",end="")
+print("Pc in accensione ... ",end="")
 send_magic_packet('1C.B7.2C.38.D8.EE', ip_address='255.255.255.255',port=9)
-print('OK')
 
 time.sleep(6)
+print('OK')
+
 
 filepath = 'connection.rdp'
 os.startfile(filepath)
